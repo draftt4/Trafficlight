43dca7271f80e9dbc836fac896d16c33b0017b95
Valentin Ivanov <valentinivanov@Air-Valentin.fritz.box>
2022-01-23 20:35:22 +0200

    add cleaner

--- task5/script.js
+++ task5/script.js
@@ -1,18 +1,34 @@
-document.querySelector('#text-add').onkeypress = function (event) {
+/*document.querySelector('#text-add').onkeypress = function (event) {
 if (event.key == 'Enter') {
        let a = document.querySelector('#text-add').value
        document.querySelector('#duplicateField').textContent = a
 
     }
-}
+}*/
+
+document.querySelector('#text-add').addEventListener('keypress' , event =>{
+    if (event.key == 'Enter') {
+        let a = document.querySelector('#text-add').value
+       document.querySelector('#duplicateField').textContent = a
+    }
+})
 
 
 document.querySelector('button').addEventListener('click' , event => {
+    event.preventDefault();
 
     let x = document.querySelector('#text-add').value;
     console.log(textContent = x);
 
 })
+
+document.querySelector('button').addEventListener('click' , event => {
+
+    let x = document.querySelector('#text-add').value = '';
+    let m = document.querySelector('#duplicateField').value = '';
+
+})
+
     
 
 
