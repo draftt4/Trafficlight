96dbe5d4b1391c0d2e02b61db582e47e6685d916
Valentin Ivanov <valentinivanov@Air-Valentin.fritz.box>
2022-01-27 13:46:25 +0200

    ready

--- task6 Modify/script.js
+++ task6 Modify/script.js
@@ -1,35 +1,19 @@
-/*const redEL = document.querySelector('#red');
-const yellowEl = document.querySelector('#yellow');
-const greenEl = document.querySelector('#green');
+const cyrcles = document.querySelectorAll('.Cyrcle');
+let activeLight = 0;
 
-function makeRed () {
-    redEL.style.background = ('red');
-    yellowEl.style.background = ('');
-    greenEl.style.background = ('');
-    yellowEl.addEventListener('click' ,makeYellow);
-    greenEl.addEventListener('click' ,makeGreen);
-    redEL.addEventListener('click' ,makeRed);
+
+
+
+function changeLight() {
+    cyrcles[activeLight].className = 'Cyrcle';
+    activeLight++;
+    if(activeLight > 2) {
+        activeLight = 0;
+    }
+    const targetLight = cyrcles[activeLight];
+    targetLight.classList.add(targetLight.getAttribute('color'))
+cyrcles[i].addEventListener('click',changeLight);
+    
 }
-redEL.addEventListener('click',makeRed);
 
-function makeYellow () {
-    redEL.style.background = ('');
-    yellowEl.style.background = ('yellow');
-    greenEl.style.background = ('');
-    greenEl.addEventListener('click' ,makeGreen);
-    redEL.addEventListener('click' ,makeRed);
-    yellowEl.addEventListener('click' ,makeYellow);
-}
-yellowEl.addEventListener('click',makeYellow);
-
-function makeGreen () {
-    redEL.style.background = ('');
-    yellowEl.style.background = ('');
-    greenEl.style.background = ('green');
-    redEL.addEventListener('click' ,makeRed);
-    yellowEl.addEventListener('click' ,makeYellow);
-    greenEl.addEventListener('click' ,makeGreen);
-}
-greenEl.addEventListener('click',makeGreen);*/
-
-
+addEventListener('click', changeLight);
